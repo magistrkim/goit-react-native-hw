9f0771ac348cee676b6bf8e55586a4f72364ed3c
Nataliya Kim <magistr1982@gmail.com>
2023-08-17 18:57:27 +0300

    Add PostsScreen user

--- src/screens/PostsScreen.jsx
+++ src/screens/PostsScreen.jsx
@@ -1,6 +1,10 @@
+import React from "react";
+import userImage from "../images/userImage.png";
 import {
   View,
   Text,
+  FlatList,
+  Image,
   KeyboardAvoidingView,
   StyleSheet,
   TouchableWithoutFeedback,
@@ -9,19 +13,55 @@ import {
 
 const PostsScreen = () => {
   return (
-    <View style={styles.wrapper}>
-      <Text>PostsScreen</Text>
-    </View>
+    <FlatList
+      style={styles.container}
+      showsVerticalScrollIndicator={false}
+      ListHeaderComponent={
+        <>
+          <View style={styles.userWrapper}>
+            <Image source={userImage} style={styles.userImage} />
+            <View>
+              <Text style={styles.userName}>Kim Nataliya</Text>
+              <Text style={styles.userEmail}>kim@gmail.com</Text>
+            </View>
+          </View>
+        </>
+      }
+    ></FlatList>
   );
 };
 
 export default PostsScreen;
 
 const styles = StyleSheet.create({
-  wrapper: {
+  container: {
     flex: 1,
     backgroundColor: "#ffffff",
     paddingTop: 32,
     paddingHorizontal: 16,
+  },
+  userWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 32,
+    gap: 8,
+  },
+  userImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 16,
+    backgroundColor: "teal",
+  },
+  userName: {
+    fontWeight: "700",
+    color: "#212121",
+    fontSize: 13,
+    lineHeight: 15,
+  },
+  userEmail: {
+    fontWeight: "400",
+    color: "#212121CC",
+    fontSize: 11,
+    lineHeight: 13,
   },
 });
